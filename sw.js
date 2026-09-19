bee03c9743b0f759b2fcd780f374d5ebfbcfd37f
kurtangelo298-design <kurtangelo298@gmail.com>
2026-09-19 16:14:18 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'studymate-ph-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css'
+];
+
+// I-install at i-cache ang mga file
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// I-update ang cache kapag may pagbabago
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// I-serve mula sa cache kapag walang internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          })
+          .catch(() => {
+            if (event.request.destination === 'document') {
+              return caches.match('./index.html');
+            }
+          });
+      })
+  );
+});
